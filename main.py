a15c535c8bee3387277585ac71f11f78b716d705
CSK <73425927+cr2007@users.noreply.github.com>
2024-09-19 17:18:46 +0400

    Refactor: move request headers from session to request level
    * Moved the `User-Agent` and `Accept` headers from the session setup to
      the individual `fetch_company_data` request to ensure flexibility.
    * Removed redundant header setup in the `create_session` function,
      simplifying session initialization.
    * Improved code readability and separation of concerns by applying
      headers directly at the request level.

--- main.py
+++ main.py
@@ -59,11 +59,6 @@ def format_company_name(company_name: str) -> str:
 def create_session() -> requests.Session:
     session = requests.Session()
 
-    session.headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/jxl,image/webp,image/png,image/svg+xml,*/*;q=0.8",
-    }
-
     retries = Retry(total=5, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
     adapter = HTTPAdapter(max_retries=retries)
     session.mount("https://", adapter)
@@ -144,12 +139,17 @@ def get_conditions(soup: BeautifulSoup) -> str:
 def fetch_company_data(session: requests.Session, company: str) -> dict[str, str]:
     loop_start_time = time.time()
 
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/jxl,image/webp,image/png,image/svg+xml,*/*;q=0.8",
+    }
+
     url = (
         f"https://www.adgm.com/public-registers/fsra/fsf/{format_company_name(company)}"
     )
 
     try:
-        response = session.get(url, timeout=10)
+        response = session.get(url, headers=headers, timeout=10)
         if response.status_code == 404:
             print(
                 f"There is a problem with the URL for {company}."
